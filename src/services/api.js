7fea329998e484145f25914a18f6b58db2f66c93
philrussel21 <phil.antiporda21@gmail.com>
2021-03-22 22:39:45 +1000

    Adds flow to get data from ingredients up to drink information

--- src/services/api.js
+++ src/services/api.js
@@ -25,6 +25,24 @@ export const getDrinksFromCat = (cat) => {
     });
 };
 
+export const getAllIng = () => {
+  const fullUrl = baseUrl + "list.php?i=list";
+  return fetch(fullUrl)
+    .then(handleErrors)
+    .then(res => {
+      return res.json();
+    });
+};
+
+export const getDrinksFromIng = (ing) => {
+  const fullUrl = baseUrl + "filter.php?i=" + ing;
+  return fetch(fullUrl)
+    .then(handleErrors)
+    .then(res => {
+      return res.json();
+    });
+};
+
 export const getDrinkFromId = (id) => {
   const fullUrl = baseUrl + "lookup.php?i=" + id;
   return fetch(fullUrl)
