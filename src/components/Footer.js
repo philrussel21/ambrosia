013ce9a0d26df324bd7583c3224f5c89b874eee1
philrussel21 <phil.antiporda21@gmail.com>
2021-03-22 15:45:01 +1000

    Initial footer design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,17 @@ import NavbarBrand from 'react-bootstrap/NavbarBrand'
 export default function Footer() {
   return (
     <div className="fixed-bottom">
-      <Navbar color="dark" dark>
-        <Container>
-          <NavbarBrand>Footer</NavbarBrand>
+      <Navbar variant="dark" bg="dark">
+        <Container className="foot-cont">
+          <NavbarBrand href="https://philantiporda.netlify.app/" target="_blank">
+            © Phil Antiporda
+          </NavbarBrand>
+          <NavbarBrand href="https://github.com/philrussel21/ambrosia" target="_blank">
+            Soure Code
+          </NavbarBrand>
+          <NavbarBrand className="foot-span">
+            Created with <a href="https://www.thecocktaildb.com/api.php" target="_blank">thecocktaildb API</a>
+          </NavbarBrand>
         </Container>
       </Navbar>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/custom.scss'
+import './styles/main.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'));
