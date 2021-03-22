0c599e2225821265a08f6f71d417e560d2722dff
philrussel21 <phil.antiporda21@gmail.com>
2021-03-22 20:38:46 +1000

    Implements routing with basic content

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,41 @@ import { Container } from 'react-bootstrap'
 import AppNav from './AppNav'
 import Disclaimer from './Disclaimer'
 import Footer from './Footer'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 const App = () => {
   return (
     <div >
-      <AppNav />
-      <Container fluid="lg" className="main-cont">
-        <Disclaimer />
-      </Container>
-      <Footer />
+      <Router>
+        <AppNav />
+        <Container fluid="lg" className="main-cont">
+          <Disclaimer />
+
+          <Switch>
+            <Route exact path="/">
+              Home
+            </Route>
+            <Route path="/categories">
+              Categories
+            </Route>
+            <Route path="/ingredients">
+              Ingredients
+            </Route>
+            <Route path="/non-alcoholic">
+              Non-Alcoholic
+            </Route>
+            <Route path="/surprise-me">
+              Surprise Me!
+            </Route>
+
+          </Switch>
+        </Container>
+        <Footer />
+      </Router>
     </div>
   )
 }
