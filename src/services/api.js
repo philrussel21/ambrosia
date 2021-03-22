dfafffe241865297cdc88e91cbc377a55717d033
philrussel21 <phil.antiporda21@gmail.com>
2021-03-22 22:34:05 +1000

    Adds flow to get data from categories up to drink information

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,35 @@
-const baseUrl = "https://www.thecocktaildb.com/api/json/v1/1/"
+const baseUrl = "https://www.thecocktaildb.com/api/json/v1/1/";
 
 // sort of like a middleware for all the reqs to check if there's any error
 // by checking if res.ok is actually OK.
 const handleErrors = (res) => {
-  if (!res.ok) throw new Error("There was an error fetching the data.")
-  return res
-}
+  if (!res.ok) throw new Error("There was an error fetching the data.");
+  return res;
+};
 
 export const getAllCat = () => {
   const fullUrl = baseUrl + "list.php?c=list";
   return fetch(fullUrl)
     .then(handleErrors)
     .then(res => {
-      res.json()
-    })
-}
+      return res.json();
+    });
+};
+
+export const getDrinksFromCat = (cat) => {
+  const fullUrl = baseUrl + "filter.php?c=" + cat;
+  return fetch(fullUrl)
+    .then(handleErrors)
+    .then(res => {
+      return res.json();
+    });
+};
+
+export const getDrinkFromId = (id) => {
+  const fullUrl = baseUrl + "lookup.php?i=" + id;
+  return fetch(fullUrl)
+    .then(handleErrors)
+    .then(res => {
+      return res.json();
+    });
+};
