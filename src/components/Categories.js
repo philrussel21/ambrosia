113a9f7a2dbef3d61a47aa3c34c30c9c3a38a887
philrussel21 <phil.antiporda21@gmail.com>
2021-03-24 12:51:39 +1000

    Initial implementation of category cards on Categories Component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Card, Container, Row, Col } from 'react-bootstrap';
 import { getAllCat } from '../services/api';
 
 export default function Categories() {
@@ -12,9 +13,42 @@ export default function Categories() {
       });
   }, []);
   return (
-    <div>
+    <div className="content">
       {console.log(categories)}
-      Categories
+      {/* Heading */}
+      <div>
+        <h1 className="display-4 text-center">
+          Categories
+        </h1>
+        <p className="lead">
+          Some text to support the catchy title. This should elaborate a little bit how to use the app or the features that it has.
+        </p>
+      </div>
+      {/* Selection */}
+      {categories ?
+        <Container>
+          <Row>
+            {categories.map((el, idx) => (
+              <Col xs={12} sm={6} key={idx} className='mx-auto mb-3 home-cards'>
+                {/* <Link to=''> */}
+
+                <Card
+                >
+                  <Card.Body>
+                    <Card.Text>
+                      {el.strCategory}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+                {/* </Link> */}
+
+              </Col>
+            ))}
+          </Row>
+        </Container>
+        :
+        // Skeleton here while loading
+        "Loading"}
     </div>
   );
 }
