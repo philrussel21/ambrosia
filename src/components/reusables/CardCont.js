b9db6bb52adf3330ebd7b1d41b9472e267938b51
philrussel21 <phil.antiporda21@gmail.com>
2021-03-26 21:49:14 +1000

    Adds CardCont and DrinkCard components for code reusability.

---
+++ src/components/reusables/CardCont.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import DrinkCard from './DrinkCard';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import AppPagination from './AppPagination';
+
+
+export default function CardCont({ currentPage, setCurrentPage, isLoading, drinks }) {
+
+  const DRINKS_PER_PAGE = 10;
+
+
+  const paginate = (pageNum) => {
+    setCurrentPage(pageNum);
+  };
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const prevPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const indexOfLastDrink = currentPage * DRINKS_PER_PAGE;
+  const indexOfFirstDrink = indexOfLastDrink - DRINKS_PER_PAGE;
+  const currentDrinks = drinks.slice(indexOfFirstDrink, indexOfLastDrink);
+
+  return (
+    <>
+      <Container>
+        <Row>
+          {isLoading
+            ?
+            // Skeleton Here
+            "Loading"
+            :
+            currentDrinks.map(drink => (
+              <DrinkCard drink={drink} key={drink.idDrink} />
+            ))
+          }
+        </Row>
+      </Container>
+      <AppPagination drinkPerPage={DRINKS_PER_PAGE} totalDrinks={drinks.length} paginate={paginate} nextPage={nextPage} prevPage={prevPage} />
+    </>
+  );
+}
--- src/components/reusables/Category.js
+++ src/components/reusables/Category.js
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { removeOriginFromPath } from '../../helpers/url_helpers';
 import { getDrinksFromCat } from '../../services/api';
 
-import { Container, Row, Col, Card } from 'react-bootstrap';
-import AppPagination from './AppPagination';
+import CardCont from './CardCont';
 
 export default function Category({ location }) {
   let category = location.pathname;
@@ -13,7 +12,7 @@ export default function Category({ location }) {
   const [drinks, setDrinks] = useState([]);
   const [isLoading, setIsLoading] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const DRINKS_PER_PAGE = 10;
+  // const DRINKS_PER_PAGE = 10;
 
   useEffect(() => {
     setIsLoading(true);
@@ -25,30 +24,8 @@ export default function Category({ location }) {
       });
   }, [category]);
 
-
-  // MOVE THE DRINK CARD COMPONENT TO A SEPERATE COMPONENT
-
-
-
-  const paginate = (pageNum) => {
-    setCurrentPage(pageNum);
-  };
-
-  const nextPage = () => {
-    setCurrentPage(currentPage + 1);
-  };
-
-  const prevPage = () => {
-    setCurrentPage(currentPage - 1);
-  };
-
-  const indexOfLastDrink = currentPage * DRINKS_PER_PAGE;
-  const indexOfFirstDrink = indexOfLastDrink - DRINKS_PER_PAGE;
-  const currentDrinks = drinks.slice(indexOfFirstDrink, indexOfLastDrink);
-
   return (
     <div className="content">
-      {console.log(currentDrinks)}
       {/* Heading */}
       <div>
         <h1 className="display-4 text-center">
@@ -59,28 +36,7 @@ export default function Category({ location }) {
         </p>
       </div>
 
-      {/* Drinks */}
-      <Container>
-        <Row>
-          {isLoading
-            ?
-            // Skeleton Here
-            "Loading"
-            :
-            currentDrinks.map(drink => (
-              <Col xs={12} sm={6} key={drink.idDrink} className='mx-auto mb-3'>
-                <Card className="mx-auto">
-                  <Card.Img variant="top" src={drink.strDrinkThumb} />
-                  <Card.Body>
-                    <Card.Text>{drink.strDrink}</Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))
-          }
-        </Row>
-      </Container>
-      <AppPagination drinkPerPage={DRINKS_PER_PAGE} totalDrinks={drinks.length} paginate={paginate} nextPage={nextPage} prevPage={prevPage} />
+      <CardCont isLoading={isLoading} drinks={drinks} currentPage={currentPage} setCurrentPage={setCurrentPage} />
     </div>
   );
 }
