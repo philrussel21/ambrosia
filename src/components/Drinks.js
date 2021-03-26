112af7ad3a54cf0c71786488754f06f7afe9954f
philrussel21 <phil.antiporda21@gmail.com>
2021-03-26 22:50:11 +1000

    Adds api call to getAllDrinks and set in state.

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { alphaSort } from '../helpers/data_helpers';
+import { getAllDrinks } from '../services/api';
 
 export default function Drinks() {
+  const [drinks, setDrinks] = useState([]);
+
+  useEffect(() => {
+    getAllDrinks()
+      .then(data => {
+        const { drinks: nonAlcDrinks } = data[0];
+        const { drinks: alcDrinks } = data[0];
+        const allDrinks = [...nonAlcDrinks, ...alcDrinks];
+        // Not necessary since .sort is destructive??
+        const sortedDrinks = alphaSort(allDrinks);
+        setDrinks(sortedDrinks);
+      });
+
+    // TODO : Print out cards with pagination for all the drinks
+  }, []);
   return (
     <div>
+      {console.log(drinks)}
       Drinks Page
     </div>
   );
