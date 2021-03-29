92ccc951ffd7669431cbe4516bd42d78a625ac50
philrussel21 <phil.antiporda21@gmail.com>
2021-03-29 21:09:35 +1000

    Prints out all Drinks card in /drinks route

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { alphaSort } from '../helpers/data_helpers';
 import { getAllDrinks } from '../services/api';
+import CardCont from './reusables/CardCont';
 
 export default function Drinks() {
   const [drinks, setDrinks] = useState([]);
+  const [isLoading, setIsLoading] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
+    setIsLoading(true);
     getAllDrinks()
       .then(data => {
         const { drinks: nonAlcDrinks } = data[0];
@@ -14,14 +18,22 @@ export default function Drinks() {
         // Not necessary since .sort is destructive??
         const sortedDrinks = alphaSort(allDrinks);
         setDrinks(sortedDrinks);
+        setIsLoading(false);
       });
-
-    // TODO : Print out cards with pagination for all the drinks
   }, []);
   return (
-    <div>
-      {console.log(drinks)}
-      Drinks Page
+    <div className="content">
+      {/* Heading */}
+      <div>
+        <h1 className="display-4 text-center">
+          All Drinks
+        </h1>
+        <p className="lead">
+          Some text to support the catchy title. This should elaborate a little bit how to use the app or the features that it has.
+        </p>
+      </div>
+
+      <CardCont isLoading={isLoading} drinks={drinks} currentPage={currentPage} setCurrentPage={setCurrentPage} />
     </div>
   );
 }
