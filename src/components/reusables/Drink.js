c2012fd7e86ed3d165ebb7944ca02ec0d8558565
philrussel21 <phil.antiporda21@gmail.com>
2021-03-29 21:22:47 +1000

    Transforms the DrinkCard component into a link to get to Drink component.

--- src/components/reusables/Drink.js
+++ src/components/reusables/Drink.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function Drink() {
+  // Extracting drinkId object since it was how it was named 
+  // in App.js component.
+  const { drinkId } = useParams();
   return (
     <div>
-      Drink
+      Drink {drinkId}
     </div>
   );
 }
