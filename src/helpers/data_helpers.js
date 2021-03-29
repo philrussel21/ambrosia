2dd7c460df370a48af97a9fec5f544408a52d313
philrussel21 <phil.antiporda21@gmail.com>
2021-03-29 22:15:49 +1000

    Adds a helper to extract ings and measurement from data to produce an array of objs instead

--- src/helpers/data_helpers.js
+++ src/helpers/data_helpers.js
@@ -7,3 +7,20 @@ export const alphaSort = (arr) => {
   });
   return arr;
 };
+
+export const extractIngsAndMeas = (obj) => {
+  const ingWithMeas = [];
+  let counter = 1;
+  while (true) {
+    const ingredient = obj[`strIngredient${counter}`];
+    const measurement = obj[`strMeasure${counter}`];
+
+    // null check
+    if (!ingredient) break;
+
+    const entry = { ingredient, measurement };
+    ingWithMeas.push(entry);
+    counter++;
+  }
+  return ingWithMeas;
+};
