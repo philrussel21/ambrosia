850dcc867142ce5343c8ea3acc812811f7be0579
philrussel21 <phil.antiporda21@gmail.com>
2021-03-30 19:37:29 +1000

    Fixes alphaSort helper to disregard up or low case when sorting.

--- src/helpers/data_helpers.js
+++ src/helpers/data_helpers.js
@@ -1,8 +1,8 @@
 
 export const alphaSort = (arr) => {
   arr.sort(function (a, b) {
-    if (a < b) { return -1; }
-    if (a > b) { return 1; }
+    if (a.toLowerCase() < b.toLowerCase()) { return -1; }
+    if (a.toLowerCase() > b.toLowerCase()) { return 1; }
     return 0;
   });
   return arr;
