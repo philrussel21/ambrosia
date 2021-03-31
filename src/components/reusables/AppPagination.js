2e29dd6e8eb07008bdd791af2578ca236395d920
philrussel21 <phil.antiporda21@gmail.com>
2021-03-31 13:00:18 +1000

    Formats pagination to only have 5 pages each time.

--- src/components/reusables/AppPagination.js
+++ src/components/reusables/AppPagination.js
@@ -10,14 +10,40 @@ export default function AppPagination({ currentPage, drinkPerPage, totalDrinks, 
   }
 
   const lastPage = pageNums[pageNums.length - 1];
+  const lastPages = pageNums.slice(Math.max(pageNums.length - 5, 0));
+
+  let pages = [];
+  if (lastPages[0] > 5) {
+    // If the current page is included in the last pages array, just print out the last 5 pages.
+    if (lastPages.includes(currentPage)) {
+      pages = lastPages.map(p => {
+        return <Pagination.Item key={p} onClick={() => paginate(p)} active={currentPage === p}>{p}</Pagination.Item>;
+      });
+    }
+    else {
+
+      for (let i = currentPage - 1; i < (currentPage + 4); i++) {
+        const p = pageNums[i];
+        const item = <Pagination.Item key={p} onClick={() => paginate(p)} active={currentPage === p}>{p}</Pagination.Item>;
+        pages.push(item);
+      }
+    }
+  }
+  // Less than 5 result pages
+  else {
+    pages = pageNums.map(p => {
+      return <Pagination.Item key={p} onClick={() => paginate(p)} active={currentPage === p}>{p}</Pagination.Item>;
+    });
+  }
 
   return (
     <>
       <Pagination>
         <Pagination.Item onClick={() => prevPage()} disabled={currentPage === 1}>Previous</Pagination.Item>
-        {pageNums.map(p => (
+        {/* {pageNums.map(p => (
           <Pagination.Item key={p} onClick={() => paginate(p)} active={currentPage === p}>{p}</Pagination.Item>
-        ))}
+        ))} */}
+        {pages}
         <Pagination.Item onClick={() => nextPage()} disabled={currentPage === lastPage}>Next</Pagination.Item>
       </Pagination>
     </>
